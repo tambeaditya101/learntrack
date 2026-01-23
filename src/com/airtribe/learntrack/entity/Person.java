ee766c394d266f7c3ef3976a91c644fcd3dd1a4e
adityatambe-personal <tambeaditya101@gmail.com>
2026-01-24 03:20:11 +0530

    Complete core entity classes with encapsulation and inheritance also demonstrate constructor overloading in Student entity

---
+++ src/com/airtribe/learntrack/entity/Person.java
@@ -0,0 +1,51 @@
+package com.airtribe.learntrack.entity;
+
+public class Person {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public Person(int id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    // getters
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // setters
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // polymorphism-friendly method
+    public String getDisplayName() {
+        return firstName + " " + lastName;
+    }
+}
--- src/com/airtribe/learntrack/ui/Main.java
+++ src/com/airtribe/learntrack/ui/Main.java
@@ -1,7 +1,23 @@
 package com.airtribe.learntrack.ui;
 
+import com.airtribe.learntrack.entity.Person;
+import com.airtribe.learntrack.entity.Student;
+
 public class Main {
     public static void main(String[] args) {
+
+        // Temporary test code – will be replaced by menu-driven UI
+
         System.out.println("LearnTrack started");
+
+        Student s1 = new Student(1, "Aditya", "Tambe", "aditya@gmail.com", "A1");
+        Student s2 = new Student(2, "Aniket", "Shinde", "B2");
+
+        System.out.println(s1.getDisplayName());
+        s1.setFirstName("Gaurav");
+        s1.setLastName("Patil");
+        System.out.println(s1.getDisplayName());
+        System.out.println(s2.getDisplayName());
+
     }
 }
