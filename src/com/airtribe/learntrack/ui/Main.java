a70b6e4af52b74a3a9e64afcbfa87c4f1a9add28
adityatambe-personal <tambeaditya101@gmail.com>
2026-01-30 01:48:01 +0530

    Add IdGenerator utility using static counters for entity IDs & InputValidator utility for basic input validation

--- src/com/airtribe/learntrack/ui/Main.java
+++ src/com/airtribe/learntrack/ui/Main.java
@@ -2,6 +2,7 @@ package com.airtribe.learntrack.ui;
 
 import com.airtribe.learntrack.entity.Person;
 import com.airtribe.learntrack.entity.Student;
+import com.airtribe.learntrack.util.IdGenerator;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,15 +11,17 @@ public class Main {
 
         System.out.println("LearnTrack started");
 
-        Student s1 = new Student(1, "Aditya", "Tambe", "aditya@gmail.com", "A1");
+        Student s1 = new Student(IdGenerator.getNextStudentId(), "Aditya", "Tambe", "aditya@gmail.com", "A1");
 
-        Person p1 = new Student(2, "Aniket", "Shinde", "B2");
+        Person p1 = new Student(IdGenerator.getNextStudentId(), "Aniket", "Shinde", "B2");
 
         System.out.println(s1.getDisplayName());
+        System.out.println(s1.getId() );
         s1.setFirstName("Gaurav");
         s1.setLastName("Patil");
         System.out.println(s1.getDisplayName());
         System.out.println(p1.getDisplayName());
+        System.out.println(p1.getId() );
 
     }
 }
--- src/com/airtribe/learntrack/util/IdGenerator.java
+++ src/com/airtribe/learntrack/util/IdGenerator.java
@@ -1,4 +1,22 @@
 package com.airtribe.learntrack.util;
 
 public class IdGenerator {
+    private static int studentIdCounter = 1;
+    private static int courseIdCounter = 1;
+    private static int enrollmentIdCounter = 1;
+
+    // Student ID
+    public static int getNextStudentId() {
+        return studentIdCounter++;
+    }
+
+    // Course ID
+    public static int getNextCourseId() {
+        return courseIdCounter++;
+    }
+
+    // Enrollment ID
+    public static int getNextEnrollmentId() {
+        return enrollmentIdCounter++;
+    }
 }
