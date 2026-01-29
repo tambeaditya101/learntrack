ed7cc84c40c0006b43f4a7709f69a3d210a7953b
adityatambe-personal <tambeaditya101@gmail.com>
2026-01-30 01:59:31 +0530

    Add StudentService with in-memory CRUD operations

--- src/com/airtribe/learntrack/entity/Person.java
+++ src/com/airtribe/learntrack/entity/Person.java
@@ -49,3 +49,5 @@ public class Person {
         return firstName + " " + lastName;
     }
 }
+
+
--- src/com/airtribe/learntrack/service/StudentService.java
+++ src/com/airtribe/learntrack/service/StudentService.java
@@ -1,4 +1,54 @@
 package com.airtribe.learntrack.service;
 
+import com.airtribe.learntrack.entity.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentService {
+
+    private List<Student> students = new ArrayList<>();
+
+    public void addStudent(Student student){
+        students.add(student);
+    }
+
+    public List<Student> getAllStudents(){
+        return  students;
+    }
+
+    public Student getStudentById(int id) {
+        for (Student student : students) {
+            if (student.getId() == id) {
+                return student;
+            }
+        }
+        return null;
+    }
+
+    public boolean updateStudent(int id, String firstName, String lastName, String batch) {
+        Student student = getStudentById(id);
+
+        if (student == null) {
+            return false;
+        }
+
+        student.setFirstName(firstName);
+        student.setLastName(lastName);
+        student.setBatch(batch);
+        return true;
+    }
+
+    public boolean deactivateStudent(int id) {
+        Student student = getStudentById(id);
+
+        if (student == null) {
+            return false;
+        }
+
+        student.deactivate();
+        return true;
+    }
+
+
 }
